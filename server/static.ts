e5874ebf2e75d5755803563a3a0adf6eb4112856
luizspinola1404 <lspinolagoncalves@gmail.com>
2026-02-23 12:27:18 -0300

    corrigindo bugs de codificação de fontes e deixando todo projeto UTF-8

--- server/static.ts
+++ server/static.ts
@@ -10,10 +10,17 @@ export function serveStatic(app: Express) {
     );
   }
 
-  app.use(express.static(distPath));
+  app.use(express.static(distPath, {
+    setHeaders: (res, filePath) => {
+      if (filePath.endsWith('.html')) {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      }
+    }
+  }));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
